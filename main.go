04a6bbc1ce1040c99b020903148b46f23f96b66e
Siyovush <paraparadox@protonmail.com>
2022-11-01 18:38:20 +0500

    Chapter 2.5: URL query strings

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with an ID of %d ...\n", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -28,7 +35,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+	w.Write([]byte("Create a new snippet...\n"))
 }
 
 func main() {

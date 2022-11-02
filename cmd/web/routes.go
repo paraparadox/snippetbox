bf991ff4dd72331e41d692badf2cd8aa913ce89d
Siyovush <paraparadox@protonmail.com>
2022-11-02 09:54:21 +0500

    Chapter 3.5: Isolating the application routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	// don't forget about 2 types of URL patterns:
+	// fixed paths and subtree paths
+	// mux.HandleFunc("/really/serious", someFunc)
+	// mux.HandleFunc("/really/serious/", otherFunc)
+
+	return mux
+}

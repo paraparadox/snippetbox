8d723078f7187ad278a4ff4167bbdd570ebbf0a5
Siyovush <paraparadox@protonmail.com>
2022-11-03 08:14:49 +0500

    Chapter 4.7: Single-record SQL queries

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/paraparadox/snippetbox/internal/models"
 	"html/template"
 	"net/http"
 	"strconv"
@@ -37,7 +39,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with an ID of %d ...\n", id)
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {

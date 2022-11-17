264dcf7d23c7d7c94adb5c5639173f62e32af630
Siyovush <paraparadox@protonmail.com>
2022-11-17 09:22:58 +0500

    Chapter 9.3: Working with session data

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -13,6 +13,7 @@ type templateData struct {
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 	Form        any
+	Flash       string
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -50,6 +51,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 

4ea0a0b74b658b5d3879f2f7994ff5d588310d3b
Siyovush <paraparadox@protonmail.com>
2022-11-21 10:56:08 +0500

    Chapter 17.6: Implement a 'Change Password' feature

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -46,3 +46,15 @@ func (m *UserModel) Get(id int) (*models.User, error) {
 		return nil, models.ErrNoRecord
 	}
 }
+
+func (m *UserModel) PasswordUpdate(id int, currentPassword, newPassword string) error {
+	if id != 1 {
+		return models.ErrNoRecord
+	}
+
+	if currentPassword != "pa$$word" {
+		return models.ErrInvalidCredentials
+	}
+
+	return nil
+}
